b2d63060ddba0b9b4590b37517c110eef48a3217
Rob Grande <robert.grande@sitechhs.com>
2020-04-21 13:17:22 -0400

    copy paste

---
+++ COPY PASTE/SuccessCombine/userRoutes.js
@@ -0,0 +1,33 @@
+//Dependencies
+const express = require('express')
+const path = require('path')
+
+
+
+
+
+//Server-side variables
+const port = process.env.port || 8888  //Default test port 8888 otherwise process environment port value generated from Heroku
+const app = express()
+
+
+
+//App use
+app.use(express.static(__dirname))
+
+
+
+//Initialize Server
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}.`)
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'inde.html'))
+})
+
+app.all('/', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", process.env.address);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next()
+  });
